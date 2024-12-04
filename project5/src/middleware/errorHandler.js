f6934ca7cd1e188392b92ac6d83fa075106da929
conner208 <156705518+conner208@users.noreply.github.com>
2024-12-04 16:39:49 -0700

    Add files via upload

---
+++ project5/src/middleware/errorHandler.js
@@ -0,0 +1,7 @@
+export function errorHandler(err, req, res, next) {
+  console.error('Error:', err.stack);
+  res.status(500).json({
+    error: 'Internal Server Error',
+    message: process.env.NODE_ENV === 'development' ? err.message : 'Something went wrong'
+  });
+}
