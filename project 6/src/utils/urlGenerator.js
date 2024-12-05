3cda42bc07bf77b385eb1c914c2df1c7571c3a75
conner208 <156705518+conner208@users.noreply.github.com>
2024-12-04 20:18:18 -0700

    Delete project 6 directory

--- project 6/src/utils/urlGenerator.js
+++
@@ -1,6 +0,0 @@
-import { format } from 'date-fns';
-
-export function generateVideoUrl(date) {
-  const formattedDate = format(date, 'MM-dd-yyyy');
-  return `https://insession.idaho.gov/IIS/2024/Senate/Chambers/SenateChambers${formattedDate}.mp4`;
-}
