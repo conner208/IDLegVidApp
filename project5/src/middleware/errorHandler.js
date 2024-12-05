83a8f9d358ccacdc5f85a2e5b0a3ed44c75e2ca7
conner208 <156705518+conner208@users.noreply.github.com>
2024-12-04 20:13:42 -0700

    Delete project5 directory

--- project5/src/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-export function errorHandler(err, req, res, next) {
-  console.error('Error:', err.stack);
-  res.status(500).json({
-    error: 'Internal Server Error',
-    message: process.env.NODE_ENV === 'development' ? err.message : 'Something went wrong'
-  });
-}
